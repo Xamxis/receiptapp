930933d5cd86e4520f8eef6d715294da4d969d89
Xamxis <xamxis12@gmail.com>
2026-09-09 16:35:59 +0000

    Serialization-Plugin ergänzt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.20"
     id("com.google.dagger.hilt.android") version "2.52"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
     id("com.google.devtools.ksp") version "2.0.20-1.0.25"
 }
 
